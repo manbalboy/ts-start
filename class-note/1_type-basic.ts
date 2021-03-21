a4fc6d63f1c7f0b92bb0bf8b162af310c92ff6df
manbalboy <manbalboy@hanmail.net>
2021-03-21 22:34:32 +0900

    타입스크립트 기본 타입 , 문자열, 숫자  , 배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+//문자열 기존 JS
+// var str = 'hello';
+// TS 문자열
+let str: string = 'hello';
+
+let num: number = 10;
+
+
+//Array 
+let arr: Array<number> = [1, 2, 3];
+let herose: Array<string> = ['a', 'b', 'c'];
+let items: number[] = [1,2,3];
+let items2: any[] = [1,2,3,"fdsa"];
