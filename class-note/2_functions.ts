2574363abfaf6ce06f5bde8a8907ce80b5998815
manbalboy <manbalboy@hanmail.net>
2021-03-21 22:50:44 +0900

    기본 함수타입

---
+++ class-note/2_functions.ts
@@ -0,0 +1,28 @@
+//함수의 파라미터에 타입을 정의하는 방식
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+sum(10, 20);
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+    return 10;
+}
+
+
+// 함수의 타입을 정의 하는 방식 f
+function sum2(a: number, b: number): number {
+    return a + b;
+}
+
+// sum2(10, 20, 30, 40);
+
+//함수의 선택적 파라미터 (옵셔널 파라미터)
+
+function log(a:string, b?:string) {
+    console.log(a, b);
+}
+
+log('a');
+log('a','b');
---
+++ class-note/function.js
@@ -0,0 +1,7 @@
+function sum99 (a,b) {
+    return a + b;
+}
+
+
+
+sum(10,20,304,0,5);
