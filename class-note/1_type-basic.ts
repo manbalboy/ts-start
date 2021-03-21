3beb7fbccdea79a22ec613a466256f3abc498ec9
manbalboy <manbalboy@hanmail.net>
2021-03-21 22:39:36 +0900

    기본 타입 튜플, object , boolean

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -3,11 +3,31 @@
 // TS 문자열
 let str: string = 'hello';
 
+//숫자
 let num: number = 10;
-
 
 //Array 
 let arr: Array<number> = [1, 2, 3];
 let herose: Array<string> = ['a', 'b', 'c'];
-let items: number[] = [1,2,3];
-let items2: any[] = [1,2,3,"fdsa"];
+let items: number[] = [1, 2, 3];
+let items2: any[] = [1, 2, 3, "fdsa"];
+
+
+//TS 튜플
+let address: [string, number] = ['gangnam', 3];
+
+
+//TS 객체
+let obj: object = {};
+let person: object = {
+    name: 'capt',
+    age: 100
+}
+
+let person2: { name: string, age: number } = {
+    name: 'dd',
+    age: 1
+}
+
+//TS 진위값
+let show: boolean = true;
