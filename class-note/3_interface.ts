82b04a0f3b6fb25099478dacc73fd35f07c916a9
manbalboy <manbalboy@hanmail.net>
2021-03-22 22:44:03 +0900

    함수에 활용한 interface

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,15 @@ interface User {
     name : string;
 }
 
+//변수에 활용한 인터페이스 
 var seho : User = {
     age : 2,
     name : 'z'
 }
+
+// 함수에 인터페이스 활용
+function getUser(user : User) {
+    console.log(user);
+}
+
+getUser({age : 12 , name:'d'});
