babc99a13dcc35f81a6cb95c1ddd875452f4d8dc
manbalboy <manbalboy@hanmail.net>
2021-03-28 11:30:02 +0900

    유니온 타입 특징

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -5,9 +5,7 @@ function logMessage(value: string) {
 logMessage('hello');
 // logMessage(100);
 
-
-
-function logMessage2 (value : string | number) {
+function logMessage2(value: string | number) {
     console.log(value);
 }
 
@@ -15,3 +13,18 @@ logMessage2('d');
 logMessage2(100);
 
 
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+//유니온 타입은 인터페이스를 받을때 공통된 속성만 인지한다.
+function askSomeone(someone : Developer | Person) {
+    someone.name
+}
