5c02ba62aff1fdc8bdee224c9cd2d7e99217dd8d
manbalboy <manbalboy@hanmail.net>
2021-03-28 11:40:08 +0900

    유니온 타입과 / 인터섹션타입의 차이점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -32,8 +32,6 @@ function askSomeone(someone : Developer | Person) {
 }
 
 
-
-
 var seho : string | number | boolean;
 var capt : string & number &  boolean;
 
@@ -46,4 +44,13 @@ function askSomeone2(someone : Developer & Person) {
 }
 
 
+
+// 유니온과 인터섹션의 차이점
+// 유니온은 타입의 선택지가 생기지만 안에 타입추론에대한 추가 로직을 해야하지만 선택지가있다
+askSomeone({name : '디벨로퍼', skill: '웹개발'});
+askSomeone({name : '캡틴', age : 22});
+
+
+//인터섹션 타입은 두가지를 합한 모든속성을 다 사용해야 사용가능
+// askSomeone2({name : '캡틴', age : 22});
 
