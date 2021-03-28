006c7f7e048767913d64d24f8ebd899a1e142c3f
manbalboy <manbalboy@hanmail.net>
2021-03-28 11:48:53 +0900

    문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,4 +1,3 @@
-
 //숫자형이넘 선언하고 아무값도 주지않으면 순서대로 index 값을 부여함
 enum Shoes {
     Nike, 
@@ -8,3 +7,15 @@ enum Shoes {
 
 let MyShoes = Shoes.Nike; // 0
 let MyShoes2 = Shoes.Adidas; // 1
+
+
+
+//문자형 이넘
+enum Shoes2 {
+    Nike = 'is Nike', 
+    Adidas = 'is Adis' 
+}
+
+
+let MyShoes3 = Shoes2.Nike; // 0
+let MyShoes4 = Shoes2.Adidas; // 1
