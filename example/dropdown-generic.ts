f7052855a994aa71cdcabd90d660ef8a498368d0
manbalboy <manbalboy@hanmail.net>
2021-03-28 12:45:18 +0900

    기본 값 타입예제

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,26 @@
-const emails: {value : string, selected: boolean}[] = [
+interface Email {
+  value: string;
+  selected: boolean;
+}
+
+const emails: Email[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts : {value : number, selected: boolean}[]= [
+interface ProductNumber {
+  value: number;
+  selected: boolean;
+}
+
+const numberOfProducts: ProductNumber[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: {value : string, selected: boolean} ) {
+function createDropdownItem(item: Email | ProductNumber) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -22,5 +32,11 @@ function createDropdownItem(item: {value : string, selected: boolean} ) {
 emails.forEach(function (email) {
   const item = createDropdownItem(email);
   const selectTag = document.querySelector('#email-dropdown');
+  selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem(product);
+  const selectTag = document.querySelector('#product-dropdown');
   selectTag.appendChild(item);
 });
