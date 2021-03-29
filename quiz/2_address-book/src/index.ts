730ea772bc4d5bff782d45aca53358b3218a599d
manbalboy <manbalboy@hanmail.net>
2021-03-29 23:45:46 +0900

    퀴즈 풀이

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -10,6 +10,12 @@ interface Contact {
   phones: PhoneNumberDictionary;
 }
 
+enum PhoneType {
+  Home = "home", 
+  Office="office",
+  Studio = "studio"
+}
+
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
 function fetchContacts(): Promise<Contact[]> {
@@ -74,25 +80,27 @@ class AddressBook {
     return this.contacts.filter(contact => contact.name === name);
   }
 
-  findContactByAddress(address) {
+  findContactByAddress(address: string): Contact[] {
     return this.contacts.filter(contact => contact.address === address);
   }
 
-  findContactByPhone(phoneNumber, phoneType: string) {
+
+
+  findContactByPhone(phoneNumber:number, phoneType: PhoneType): Contact[]  {
     return this.contacts.filter(
       contact => contact.phones[phoneType].num === phoneNumber
     );
   }
 
-  addContact(contact) {
+  addContact(contact:Contact) : void {
     this.contacts.push(contact);
   }
 
-  displayListByName() {
+  displayListByName():string[] {
     return this.contacts.map(contact => contact.name);
   }
 
-  displayListByAddress() {
+  displayListByAddress():string[] {
     return this.contacts.map(contact => contact.address);
   }
   /* ------------------------------------------------ */
